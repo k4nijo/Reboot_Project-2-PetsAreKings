bef6dc00fdbbcc620078114eea5decb3308d07d2
Sara Suárez <sara.su.guerra@gmail.com>
2022-02-16 20:27:43 +0000

    Fixed booking bugs

--- api/controllers/booking.controller.js
+++ api/controllers/booking.controller.js
@@ -3,7 +3,7 @@ const BookingModel = require('../models/booking.model')
 
 async function createBooking(req, res) {
   try {
-    const user = await UserModel.findById(req.params.user_id)
+    const user = await UserModel.findById(req.params.host_id)
     const newFromDate = new Date(req.body.from_date)
     const newToDate = new Date(req.body.to_date)
     const fromDate = user.host.available_dates.from_date
@@ -11,7 +11,7 @@ async function createBooking(req, res) {
 
     if (newFromDate.getTime() >= fromDate.getTime() && newFromDate.getTime() <= toDate.getTime() &&
       newToDate.getTime() >= fromDate.getTime() && newToDate.getTime() <= toDate.getTime()) {
-      BookingModel.create({
+      const booking = await BookingModel.create({
         from_date: newFromDate,
         to_date: newToDate,
         user_id: res.locals.user.user_id,
@@ -19,9 +19,13 @@ async function createBooking(req, res) {
         pet_id: req.body.pet_id,
         createdAt: new Date()
       })
+      user.bookings.push(booking)
+      user.save()
       res.send('Booking successfully completed')
+    } else {
+      res.send('Error fecha no disponible')
     }
-    res.send('Error fecha no disponible')
+   
   } catch (error) {
     res.status(500).send(`Request error: ${error}`)
   }
@@ -29,40 +33,43 @@ async function createBooking(req, res) {
 
 async function getAllBooking(req, res) {
   try {
-    const booking = await BookingModel.findById(req.params.user_id)
-
-    if (booking) {
-      res.status(200).json(user.booking)
+    const user = await UserModel.findById(req.params.user_id).populate('bookings')
+    console.log(user)
+    if (user) {
+      res.status(200).json(user.bookings)
     } else {
-      res.status(404).json(error)
+      res.status(404).json("User not found")
     }
-
   } catch (error) {
     res.status(500).send(`Request error: ${error}`)
   }
 }
 
-async function updateBooking(req, res) {
+/*async function updateBooking(req, res) {
   try {
     const booking = await BookingModel.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
     res.status(200).json({ message: `Your booking is updated!`, booking })
   } catch (error) {
     res.status(500).send(`Request Error: ${error}`)
   }
-}
+}*/
 
 async function deleteBooking(req, res) {
   try {
-    const booking = await BookingModel.findByIdAndDelete(req.params.id)
-    res.status(200).send(`Your booking is deleted`)
+    const user = await User.findById(req.params.userid)
+    user.bookings.remove(req.params.bookingid)
+    user.save()
+    const booking = await BookingModel.findByIdAndDelete(req.params.bookingid)
+    res.status(200).send(`Booking deleted`)
   } catch (error) {
     res.status(500).send(`Request Error: ${error}`)
   }
 }
 
+
 module.exports = {
   createBooking,
   getAllBooking,
-  updateBooking,
+  //updateBooking,
   deleteBooking
 }
